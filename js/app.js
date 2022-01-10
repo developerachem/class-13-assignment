683ff0860515d108bd1902778ef9e70353b3120a
developerachem <developerachem@gmail.com>
2022-01-09 16:09:29 -0800

    replace a new file

--- js/app.js
+++ js/app.js
@@ -6,13 +6,39 @@ const day = document.querySelector('#day');
 const tarik = document.querySelector('#date');
 
 
-setInterval( () => {
+setInterval(() => {
     let date = new Date();
+    let h = date.getHours() === 0 ? 12 : date.getHours();
     day.innerHTML = weeck(date.getDay())
     ampm.innerHTML = amPmCal(date.getHours())
-    hours.innerHTML = zeroAdd(date.getHours() > 12 ? date.getHours() - 12 : date.getHours() )
+    hours.innerHTML = zeroAdd(h > 12 ? h - 12 : h)
     minit.innerHTML = zeroAdd(date.getMinutes())
     secend.innerHTML = zeroAdd(date.getSeconds())
-    tarik.innerHTML = `${zeroAdd(date.getDay())}/${zeroAdd(date.getMonth() == date.getMonth() ? date.getMonth() + 1 : date.getMonth()) }/${date.getFullYear()}`
-    
+    tarik.innerHTML = `${zeroAdd(date.getDate())}/${zeroAdd(date.getMonth() == date.getMonth() ? date.getMonth() + 1 : date.getMonth()) }/${date.getFullYear()}`
+
+
+
+
+
+
+}, 1000)
+
+setInterval(() => {
+    // 2nd Watch
+    let date = new Date();
+    const hours2 = document.querySelector('#hours2');
+    const munit2 = document.querySelector('#munit2');
+    const secend2 = document.querySelector('#secend2');
+    const ampm2 = document.querySelector('.ampm2');
+    const day2 = document.querySelector('.day2');
+
+    secend2.innerHTML = zeroAdd(date.getSeconds());
+    munit2.innerHTML = zeroAdd(date.getMinutes())
+    day2.children[1].innerHTML = `${zeroAdd(date.getDate())}/${monthCal(date.getMonth()) }`
+    day2.children[0].innerHTML = weeck2(date.getDay())
+    hours2.innerHTML = zeroAdd(date.getHours() > 12 ? date.getHours()  - 12 : date.getHours() )
+    ampm2.innerHTML = date.getHours() > 12 ? "PM" : "AM";
+
+
+
 },1000)
--- js/function.js
+++ js/function.js
@@ -50,3 +50,54 @@ function weeck (day) {
         return 'Saturday'
     }
 }
+
+
+function weeck2 (day) {
+    if( day === 0 ){
+        return 'Sun'
+    }else if( day === 1 ){
+        return 'Mon'
+    }else if( day === 2 ){
+        return 'Tue'
+    }else if( day === 3 ){
+        return 'Wed'
+    }else if( day === 4 ){
+        return 'Thu'
+    }else if( day === 5 ){
+        return 'Fri'
+    }else if( day === 6 ){
+        return 'Sat'
+    }
+}
+/**
+ * Month Function
+ * @param {*} month 
+ * @returns 
+ */
+function monthCal (month) {
+    if( month === 0 ){
+        return 'Jan'
+    }else if( month === 1 ){
+        return 'Feb'
+    }else if( month === 2 ){
+        return 'Mar'
+    }else if( month === 3 ){
+        return 'Apr'
+    }else if( month === 4 ){
+        return 'May'
+    }else if( month === 5 ){
+        return 'Jun'
+    }else if( month === 6 ){
+        return 'Jul'
+    }else if( month === 7 ){
+        return 'Agu'
+    }else if( month === 8 ){
+        return 'Sep'
+    }else if( month === 9 ){
+        return 'Oct'
+    }else if( month === 10 ){
+        return 'Nov'
+    }else if( month === 11 ){
+        return 'Dec'
+    }
+}
